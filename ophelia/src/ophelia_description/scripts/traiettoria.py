c00d9db1e6c1bd1b735fddd1bbbffc4ae0a964c0
Tambup <alberto.tamburini@hotmail.it>
2021-08-17 11:31:13 +0200

    Traiettoria improved

--- ophelia/src/ophelia_description/scripts/traiettoria.py
+++ ophelia/src/ophelia_description/scripts/traiettoria.py
@@ -1,57 +1,41 @@
 #! /usr/bin/env python
 
 import rospy
-import math
+from math import pow, sqrt, atan, asin, acos, pi
 
-a = 49.8  # lungheza anca
-f = 76.2  # lunghezza femore
-t = 136.4  # lunghezza tibia
-
+# start 135 0 -31
 x = 135.0
 y = 0.0
 z = -31.0
 
-# start 135 0 -31
-
 
 class Cinematica:
-    def __init__(self):
-        pass
+    __slots__ = 'a', 'f', 't'
+
+    def __init__(self, anca=49.8, femore=76.2, tibia=136.4):
+        self.a = anca
+        self.f = femore
+        self.t = tibia
 
     def calcoloIK_joint1(self, x, y, z):
-        self.x = x  # pos X
-        self.y = y  # pos Y
-        self.z = z  # pos Z
         # calcolo angolo c=gamma
-        c = math.atan(y/x)
+        c = atan(y/x)
         th1_r = c  # theta1 in radianti anca
         return(th1_r)
 
     def calcoloIK_joint2(self, x, y, z):
-        self.x = x  # pos X
-        self.y = y  # pos Y
-        self.z = z  # pos Z
         # calcoli angoli a1=alfa1 e a2=alfa2
-        l1 = (math.sqrt(math.pow(x, 2)+math.pow(y, 2)))-a
-        l2 = math.sqrt(math.pow(l1, 2)+math.pow(z, 2))
-        a1 = math.asin((l1)/(l2))
-        a2 = math.acos(
-            (math.pow(f, 2)-math.pow(t, 2)+math.pow(l2, 2))/(2*f*l2))
-        th2_r = (a1+a2)-(math.pi/2)  # theta2 in radianti femore
+        l1 = (sqrt(pow(x, 2)+pow(y, 2)))-self.a
+        l2 = sqrt(pow(l1, 2)+pow(z, 2))
+        a1 = asin((l1)/(l2))
+        a2 = acos((pow(self.f, 2)-pow(self.t, 2)+pow(l2, 2)) / (2*self.f*l2))
+        th2_r = (a1+a2)-(pi/2)  # theta2 in radianti femore
         return(th2_r)
 
     def calcoloIK_joint3(self, x, y, z):
-        self.x = x  # pos X
-        self.y = y  # pos Y
-        self.z = z  # pos Z
         # calcolo angolo b=beta
-        b = math.acos((math.pow(t, 2)+math.pow(f, 2)-math.pow(x, 2)-math.pow(y, 2)-math.pow(
-            a, 2)-math.pow(z, 2)+(2*a*math.sqrt(math.pow(x, 2)+math.pow(y, 2))))/(2*f*t))
-        th3_r = -((math.pi/2)-b)  # theta2 in radianti femore
-        return(th3_r)
-
-# puntoUno=Cinematica()
-# a1=puntoUno.calcoloIK_joint1(135, 0, -31)
-# a2=puntoUno.calcoloIK_joint2(135, 0, -31)
-# a3=puntoUno.calcoloIK_joint3(135, 0, -31)
-# print(a1,a2,a3)
+        temp = pow(self.t, 2)+pow(self.f, 2)-pow(x, 2)-pow(y, 2) \
+            - pow(self.a, 2)-pow(z, 2)+(2*self.a*sqrt(pow(x, 2)+pow(y, 2)))
+        b = acos(temp/(2*self.f*self.t))
+        th3_r = -((pi/2)-b)  # theta2 in radianti femore
+        return th3_r
