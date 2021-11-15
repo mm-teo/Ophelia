accab9863efae707da573421473dad342443ba9e
Tambup <alberto.tamburini@hotmail.it>
2021-11-15 18:11:55 +0100

    Adding script to send messages to raspi and the script to send the message to the adafruit.

--- src/ophelia_description/scripts/send_to_adafruit.py
+++ src/ophelia_description/scripts/send_to_adafruit.py
@@ -1,87 +1,52 @@
-#!/usr/bin/env python3
-
-from math import degrees
 import rospy
-from syropod_highlevel_controller.msg import LegState
 from adafruit_servokit import ServoKit
+from random.msg import LegPos
 
 
-class Servos:
-    __slots = 'left', 'right'
+right = ServoKit(channels=16, address=0x40)
+left = ServoKit(channels=16, address=0x41)
 
-    def __init__(self):
-        self.right = ServoKit(channels=16, address=0x40)
-        self.left = ServoKit(channels=16, address=0x41)
 
-    def set_left_arm(self, indexes, values):
-        self.left.servo[indexes[0]].angle = values[0]
-        self.left.servo[indexes[1]].angle = values[1]
-        self.left.servo[indexes[2]].angle = values[2]
+def set_left_arm(self, indexes, values):
+    left.servo[indexes[0]].angle = values[0]
+    left.servo[indexes[1]].angle = values[1]
+    left.servo[indexes[2]].angle = values[2]
 
-    def set_right_arm(self, indexes, values):
-        self.right.servo[indexes[0]].angle = values[0]
-        self.right.servo[indexes[1]].angle = values[1]
-        self.right.servo[indexes[2]].angle = values[2]
 
-    def set_joints(self, arm_state, arm_name):
-        arm_state = [degrees(val) for val in arm_state.joint_positions]
+def set_right_arm(self, indexes, values):
+    right.servo[indexes[0]].angle = values[0]
+    right.servo[indexes[1]].angle = values[1]
+    right.servo[indexes[2]].angle = values[2]
 
-        if arm_name[1] == 'L':
-            if arm_name[0] == 'A':
-                self.set_left_arm(indexes=[13, 14, 15], values=arm_state)
-            elif arm_name[0] == 'B':
-                self.set_left_arm(indexes=[5, 6, 7], values=arm_state)
-            elif arm_name[0] == 'C':
-                self.set_left_arm(indexes=[2, 1, 0], values=arm_state)
-        elif arm_name[1] == 'R':
-            if arm_name[0] == 'A':
-                self.set_right_arm(indexes=[13, 14, 15], values=arm_state)
-            elif arm_name[0] == 'B':
-                self.set_right_arm(indexes=[5, 6, 7], values=arm_state)
-            elif arm_name[0] == 'C':
-                self.set_right_arm(indexes=[2, 1, 0], values=arm_state)
+
+def set_joints(msg):
+    leg_name = msg.legName
+    leg_state = [msg.coxa, msg.femur, msg.tibia]
+    if leg_name[1] == 'L':
+        if leg_name[0] == 'A':
+            set_left_arm(indexes=[13, 14, 15], values=leg_state)
+        elif leg_name[0] == 'B':
+            set_left_arm(indexes=[5, 6, 7], values=leg_state)
+        elif leg_name[0] == 'C':
+            set_left_arm(indexes=[2, 1, 0], values=leg_state)
+    elif leg_name[1] == 'R':
+        if leg_name[0] == 'A':
+            set_right_arm(indexes=[13, 14, 15], values=leg_state)
+        elif leg_name[0] == 'B':
+            set_right_arm(indexes=[5, 6, 7], values=leg_state)
+        elif leg_name[0] == 'C':
+            set_right_arm(indexes=[2, 1, 0], values=leg_state)
 
 
 def main():
     rospy.init_node(name='send_to_adafruit')
-    rate = rospy.Rate(10)
-    servos = Servos()
+    rate = rospy.Rate(20)
+    servos = Servos(rospy)
 
-    rospy.Subscriber(name='/shc/AL/state',
-                     data_class=LegState,
-                     callback=servos.set_joints,
-                     callback_args='AL',
-                     queue_size=1)
-
-    rospy.Subscriber(name='/shc/AR/state',
-                     data_class=LegState,
-                     callback=servos.set_joints,
-                     callback_args='AR',
-                     queue_size=1)
-
-    rospy.Subscriber(name='/shc/BL/state',
-                     data_class=LegState,
-                     callback=servos.set_joints,
-                     callback_args='BL',
-                     queue_size=1)
-
-    rospy.Subscriber(name='/shc/BR/state',
-                     data_class=LegState,
-                     callback=servos.set_joints,
-                     callback_args='BR',
-                     queue_size=1)
-
-    rospy.Subscriber(name='/shc/CL/state',
-                     data_class=LegState,
-                     callback=servos.set_joints,
-                     callback_args='CL',
-                     queue_size=1)
-
-    rospy.Subscriber(name='/shc/CR/state',
-                     data_class=LegState,
-                     callback=servos.set_joints,
-                     callback_args='CR',
-                     queue_size=1)
+    rospy.Subscriber(name='/raspi_command',
+                     data_class=LegPos,
+                     callback=set_joints,
+                     queue_size=10)
 
     rospy.spin()
 
---
+++ src/ophelia_description/scripts/send_to_raspi.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+from math import degrees
+import rospy
+from ophelia_description.msg import LegPos
+from syropod_highlevel_controller.msg import LegState
+
+
+class Servos:
+    __slots = 'pub'
+
+    def __init__(self, rospy):
+        self.pub = rospy.Publisher(name='raspi_command',
+                                   data_class=LegPos,
+                                   queue_size=10)
+
+    def set_joints(self, arm_state, arm_name):
+        arm_state = [degrees(val) for val in arm_state.joint_positions]
+
+        msg = LegPos()
+        msg.legName = arm_name
+        msg.coxa = arm_state[0]
+        msg.femur = arm_state[1]
+        msg.tibia = arm_state[2]
+        self.pub.publish(msg)
+
+
+def main():
+    rospy.init_node(name='send_to_raspi')
+    rate = rospy.Rate(10)
+    servos = Servos(rospy)
+
+    rospy.Subscriber(name='/shc/AL/state',
+                     data_class=LegState,
+                     callback=servos.set_joints,
+                     callback_args='AL',
+                     queue_size=1)
+
+    rospy.Subscriber(name='/shc/AR/state',
+                     data_class=LegState,
+                     callback=servos.set_joints,
+                     callback_args='AR',
+                     queue_size=1)
+
+    rospy.Subscriber(name='/shc/BL/state',
+                     data_class=LegState,
+                     callback=servos.set_joints,
+                     callback_args='BL',
+                     queue_size=1)
+
+    rospy.Subscriber(name='/shc/BR/state',
+                     data_class=LegState,
+                     callback=servos.set_joints,
+                     callback_args='BR',
+                     queue_size=1)
+
+    rospy.Subscriber(name='/shc/CL/state',
+                     data_class=LegState,
+                     callback=servos.set_joints,
+                     callback_args='CL',
+                     queue_size=1)
+
+    rospy.Subscriber(name='/shc/CR/state',
+                     data_class=LegState,
+                     callback=servos.set_joints,
+                     callback_args='CR',
+                     queue_size=1)
+
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
